d525348098b29f070129e64727e6aeb206386e7f
Bhaskar Murthy <bhaskar.murthy@getmaple.ca>
2021-02-21 13:40:06 -0500

    chore: fix typescript missing import typo

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import CodePushProvider, {
   CodePushSplashScreen,
 } from 'react-native-code-push-utils';
